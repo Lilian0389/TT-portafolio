e47180881a263f1e15eb1841e885143922ad695f
Lilian <lilian.davilag@gmail.com>
2024-03-28 19:29:06 -0600

    Se agrego mas codigo

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ import streamlit as st
 import time
 
 st.header('Lanzar una moneda')
-number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10, key='number_of_trials_slider')
-start_button = st.button('Ejecutar', key='start_button')
-if start_button:
-    st.write(f'Experimento con {number_of_trials} intentos en curso.')
-st.write('Esta aplicación aún no es funcional. En construcción.')
+
 chart = st.line_chart([0.5])
+
 def toss_coin(n):
-    trial_outcomes
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
     mean = None
     outcome_no = 0
     outcome_1_count = 0
+
     for r in trial_outcomes:
         outcome_no +=1
         if r == 1:
@@ -21,8 +21,12 @@ def toss_coin(n):
         mean = outcome_1_count / outcome_no
         chart.add_rows([mean])
         time.sleep(0.05)
+
     return mean
+
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
+
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    mean = toss_coin(number_of_trials)
